222899cf68db80a56ca7c5530ce5f82df91e9e8e
victor schmoegel <victorschmoegel@gmail.com>
2024-11-23 16:30:49 -0300

    criado endpoint GET

--- api/src/controllers/ride.controller.ts
+++ api/src/controllers/ride.controller.ts
@@ -56,14 +56,84 @@ export const confirmRideController = async (req: Request, res: Response): Promis
 
     // Salvar a viagem no banco de dados
     await query(
-      `INSERT INTO rides (customer_id, origin, destination, distance, duration, driver_id, driver_name, value)
-       VALUES (?, ?, ?, ?, ?, ?, ?, ?)`,
-      [customer_id, origin, destination, distance, duration, driver.id, driver.name, value]
+      `INSERT INTO rides (customer_id, data, origin, destination, distance, duration, driver_id, driver_name, value)
+       VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)`,
+      [customer_id, new Date(), origin, destination, distance, duration, driver.id, driver.name, value]
     );
 
     res.status(200).json({ message: 'Operação realzada com sucesso', success: true });
   } catch (error: any) {
     console.error('Erro ao confirmar viagem:', error.message);
+    res.status(500).json({
+      error_code: 'INTERNAL_ERROR',
+      error_description: 'Erro interno no servidor.',
+    });
+  }
+};
+
+export const getRidesController = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const { customer_id } = req.params;
+    const { driver_id } = req.query;
+
+    if (!customer_id) {
+      res.status(400).json({
+        error_code: 'INVALID_DATA',
+        error_description: 'O ID do cliente não pode estar em branco',
+      });
+      return;
+    }
+
+    if (driver_id) {
+      const [driverExists]: any = await query('SELECT * FROM drivers WHERE id = ?', [driver_id]);
+      if (!driverExists.length) {
+        res.status(400).json({
+          error_code: 'DRIVER_NOT_FOUND',
+          error_description: 'Motorista inválido',
+        });
+        return;
+      }
+    }
+
+    const sql = `
+      SELECT r.id, r.data, r.origin, r.destination, r.distance, r.duration, 
+      r.value, d.id AS driver_id, d.name AS driver_name
+      FROM rides r
+      INNER JOIN drivers d ON r.driver_id = d.id
+      WHERE r.customer_id = ?
+      ${driver_id ? "AND r.driver_id = ?" : ""}
+      ORDER BY r.data DESC
+    `;
+    const params = driver_id ? [customer_id, driver_id] : [customer_id];
+    const [rides]: any = await query(sql, params);
+
+    if (!rides.length) {
+      res.status(404).json({
+        error_code: 'NO_RIDES_FOUND',
+        error_description: 'Nenhum registro encontrado',
+      });
+      return;
+    }
+
+    const formattedRides = rides.map((ride: any) => ({
+      id: ride.id,
+      date: ride.data,
+      origin: ride.origin,
+      destination: ride.destination,
+      distance: ride.distance,
+      duration: ride.duration,
+      driver: {
+        id: ride.driver_id,
+        name: ride.driver_name,
+      },
+      value: ride.value,
+    }));
+    res.status(200).json({
+      customer_id,
+      rides: formattedRides,
+    });
+  } catch (error: any) {
+    console.error('Erro ao listar viagens', error.message);
     res.status(500).json({
       error_code: 'INTERNAL_ERROR',
       error_description: 'Erro interno no servidor.',
--- api/src/routes/ride.routes.ts
+++ api/src/routes/ride.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { estimateRideController, confirmRideController } from "../controllers/ride.controller";
+import { estimateRideController, confirmRideController, getRidesController } from "../controllers/ride.controller";
 
 const router = Router();
 
 router.post('/ride/estimate', estimateRideController);
 router.patch('/ride/confirm', confirmRideController);
+router.get("/ride/:customer_id", getRidesController);
 
 export default router;
