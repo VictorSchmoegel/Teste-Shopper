cba9e1bac1ffe3cc5429acfbea479973f853a84b
victor schmoegel <victorschmoegel@gmail.com>
2024-11-27 19:32:53 -0300

    alterando dockfile da api para executar o comando npx tsc

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,11 +1,12 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 import rideRoutes from './routes/ride.routes';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
 const app = express();
-app.use(express.json());
+app.use(bodyParser.json());
 app.use('/api', rideRoutes);
 
 export default app;
